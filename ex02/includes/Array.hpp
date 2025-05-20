ba466e7889567923a89e5268863162dd5ada3903
vikingokvist <vikingokvist@gmail.com>
2025-05-20 08:36:40 +0200

    Update Array.hpp

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -1,28 +1,35 @@
 #ifndef ARRAY_HPP
-#define ARRAY_HPP
+# define ARRAY_HPP
 
 #include <iostream>
 #include <string>
 #include <exception>
 
+template <typename T>
 class Array
 {
-    private:
+	private:
+		T *array;
+		unsigned int size;
 
-    public:
-        Array();
-        Array(unsigned int n);
-        Array(const Array& src);
-        Array& operator=(const Array& src);
-        ~Array();
-
-        class OutOfBoundsExceeption : public std::exception {
-            public:
-                const char* what() const throw();
-        };
-
-        
-
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array& src);
+		Array& operator=(const Array& src);
+		~Array();
+		
+		T& operator[](unsigned int index);
+		
+		unsigned int size() const;
+		
+		class OutOfBoundsException : std::exception {
+			public:
+				const char* what() const trow();
+		}
 };
+
+# include "./Array.tpp"
+
 
 #endif
